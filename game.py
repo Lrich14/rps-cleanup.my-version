ef06de7a26086f289154da8f38c4f9a5e2e5cdd2
Lrich14 <lr1696@stern.nyu.edu>
2022-07-21 20:36:58 -0400

    winner function returns some things and can be tested

--- game.py
+++ game.py
@@ -10,6 +10,26 @@ from random import choice
 valid_choices = ["rock", "paper", "scissors"]
 
 def winner(user_choice, computer_choice):
+    if user_choice == "rock" and computer_choice == "rock":
+        return "It's a tie!"
+    elif user_choice == "rock" and computer_choice == "paper":
+        return "The computer wins"
+    elif user_choice == "rock" and computer_choice == "scissors":
+        return "The user wins"
+
+    elif user_choice == "paper" and computer_choice == "rock":
+        return "The computer wins"
+    elif user_choice == "paper" and computer_choice == "paper":
+        return "It's a tie!"
+    elif user_choice == "paper" and computer_choice == "scissors":
+        return "The user wins"
+
+    elif user_choice == "scissors" and computer_choice == "rock":
+        return "The computer wins"
+    elif user_choice == "scissors" and computer_choice == "paper":
+        return "The user wins"
+    elif user_choice == "scissors" and computer_choice == "scissors":
+        return "It's a tie!"
     return "OOPS - TODO"
 
 # only run this code if we run this script from the command line
